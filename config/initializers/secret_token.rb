13c6dd2a426ac1ed951007152ae036dd9998f8e6
Leif <lm@bluebox.de>
2013-02-12 13:43:07 +0100

    Demo App INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '0c35d372a53e03b622ef0421fd6e3579125c220ad089ee7bf86f25742f1eacab84c5e156b4ebd3b6896db116feaac0ea26ca7448f22c436df560b0c938e8e4b1'
